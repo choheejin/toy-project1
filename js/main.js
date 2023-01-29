dcb60702e9546b7f1a3ebc93b20471b1d6d37667
choheejin <hjcho9510@gmail.com>
2023-01-29 23:16:26 +0900

    주소 잘못 기입 오류 해결

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ function clickButton() {
     const color = document.getElementById('color').value.toString();
     const sliceColor = color.slice(1);
     const title = document.getElementById('text').value;
-    location.href = `/toyproject1/preview.html?title=${title}&color=${sliceColor}`;
+    location.href = `/toy-project1/preview.html?title=${title}&color=${sliceColor}`;
 }
